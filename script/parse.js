d565ebaedc7b33a7b0b0cc446902d66eb7129812
isboyjc <214930661@qq.com>
2024-08-02 02:23:21 +0800

    test✅: test lang json

--- script/parse.js
+++ script/parse.js
@@ -15,12 +15,15 @@ function parse(data){
     const stars = $(elm).find(`a[href=${url + '/stargazers'}]`).text().replace(/\n/g, '').trim()
     const forks = $(elm).find(`a[href=${url + '/forks'}]`).text().replace(/\n/g, '').trim()
     const addStars = $(elm).find('span').last().text().match(/\d+/g)[0]
-    const contributors = $(elm).find(`a[data-hovercard-type="user"]`).map((_ ,user) => {
-      const name = $(user).find('.avatar').attr('alt').replace(/^@/, '')
+    const contributors = $(elm).find(`a[data-hovercard-type="user"] .avatar.avatar-user`).map((_ ,user) => {
+      const name = $(user).attr('alt').replace(/^@/, '')
+      // const url = BASE_URL + $(user).attr('href')
+      const url = `${BASE_URL}/${name}`
+      // console.log(url)
       return {
-        avatar: $(user).find('.avatar.avatar-user').attr('src'),
+        avatar: $(user).attr('src'),
         name,
-        url: `https://github.com/${name}`
+        url
       }
     }).get();
     const item = {
